e53f417dc958edba79f217b2bebeafc8a4fbaae6
jjcampaniello-sys <jj.campaniello@gmail.com>
2026-08-23 21:39:34 +0200

    Add caching and fetch handling in service worker
    Implement service worker caching for assets and handle fetch requests.

--- sw.js
+++ sw.js
@@ -1,9 +1,38 @@
-self.addEventListener('install', (e) => {
-    self.skipWaiting();
+const CACHE_NAME = 'effiprot-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './readme.html',
+  './app.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => key !== CACHE_NAME ? caches.delete(key) : null))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        fetch(event.request).catch(() => caches.match(event.request))
-    );
+  event.respondWith(
+    fetch(event.request)
+      .then((networkResponse) => {
+        const clone = networkResponse.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        return networkResponse;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
